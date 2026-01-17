5ff3e2e4337df08770b1f7d860bcbf3e2208483f
ryokf <ryokhrisnaf@gmail.com>
2026-01-17 14:15:42 +0700

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from src.data_loader import load_and_clean_data, get_features_target
-from src.model import train_linear_regression, evaluate_model
+from src.model import train_linear_regression, evaluate_model, train_polynomial_regression, train_lasso_regression
 
 def main():
     df = load_and_clean_data('data/car_data.csv')
     
     x, y = get_features_target(df)
     
+    print("Hasil linier regression : ")
     model, X_test, y_test = train_linear_regression(x, y)
+    evaluate_model(model, X_test, y_test)
     
-    evaluate_model(model, X_test, y_test, x.columns)
+    print("Hasil polynomial regression : ")
+    model, X_test, y_test = train_polynomial_regression(x, y)
+    evaluate_model(model, X_test, y_test)
+    
+    print("Hasil lasso regression : ")
+    model, X_test, y_test = train_lasso_regression(x, y)
+    evaluate_model(model, X_test, y_test)
+    
     
 if __name__ == "__main__":
     main()
--- src/model.py
+++ src/model.py
@@ -1,8 +1,30 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import r2_score, mean_squared_error
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+def train_lasso_regression(x,y):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    
+    model = Lasso(alpha=1, max_iter=10000)
+    
+    model.fit(X_train, y_train)
+    
+    return model, X_test, y_test
+
+def train_polynomial_regression(x,y):
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    
+    model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression(), memory=None)
+    
+    model.fit(X_train,y_train)
+    
+    return model, X_test, y_test
+    
 
 def train_linear_regression(x, y):
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
@@ -12,16 +34,18 @@ def train_linear_regression(x, y):
     
     return model, X_test, y_test
 
-def evaluate_model(model, X_test, y_test, feature_names):
+def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
+    
+    plt.figure(figsize=(10,6))
+    plt.scatter(y_test, y_pred, color="blue")
+    plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2)
+    plt.xlabel('harga asli')
+    plt.ylabel('harga prediksi')
 
-    pd.options.display.float_format = '{:,.3f}'.format
-    df_coef = pd.DataFrame({'Feature': feature_names, 'Coefficient': model.coef_.flatten()})
-    print(df_coef.sort_values(by='Coefficient', ascending=False).to_string())
+    # plt.show()
 
-    plt.scatter(X_test['Mileage'], y_test, color='blue', label='actual')
-    plt.plot(X_test['Mileage'], y_pred, color='red', label='predicted')
-    plt.show()
-
-    score = r2_score(y_test, y_pred)
-    print(f"R2 Score (Akurasi): {score:.2f}")
+    r2 = r2_score(y_test, y_pred)
+    rmse_score = np.sqrt(mean_squared_error(y_test, y_pred))
+    print(f"R2 Score (Akurasi): {r2:.2f}")
+    print(f"RMSE: {rmse_score:.2f}")
